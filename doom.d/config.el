d35266f26b10b40a785d1dca021c63a9b5131c71
Devarsh Anilkumar <104669945+Devarsh-Anilkumar@users.noreply.github.com>
2022-05-05 15:59:17 +0530

    Update config.el

--- doom.d/config.el
+++ doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Shloomy Shloms"
-      user-mail-address "shloomyshloms@shloomyshloms.com")
+(setq user-full-name "Devarsh Anilkumar"
+      user-mail-address "Devarshanilkumar0@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -34,7 +34,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-city-lights)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
