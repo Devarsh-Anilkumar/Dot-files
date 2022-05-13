41c27b0be0f09563b561ce8d0975dd425f14fe9f
Devarsh Anilkumar <104669945+Devarsh-Anilkumar@users.noreply.github.com>
2022-05-13 13:10:53 +0530

    Update config.el

--- doom.d/config.el
+++ doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Devarsh Anilkumar"
-      user-mail-address "Devarshanilkumar0@gmail.com")
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
